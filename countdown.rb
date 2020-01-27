43e4b242d264b74c6b68c3bbe19beacaf600f7e7
Jeffery Eckert <bromodo@gmail.com>
2020-01-27 02:35:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,7 @@ end
 def countdown_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 0 
   puts "#{seconds_to_midnight} SECOND(S)!"
+  sleep 
     seconds_to_midnight -= 1
    end
        "HAPPY NEW YEAR!"
